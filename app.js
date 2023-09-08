f6cd38dac44b5e4571c5108badfe017d774e4178
Daniel Timileyin <111635840+tiimyy@users.noreply.github.com>
2023-09-08 17:04:02 +0100

    Update app.js

--- app.js
+++ app.js
@@ -10,15 +10,13 @@ function updateDayOfWeek() {
 updateDayOfWeek(); // Initial update
 setInterval(updateDayOfWeek, 1000 * 60 * 60 * 24);
 
-function updateUTC() {
-    const now = new Date();
-    const hours = now.getUTCHours().toString().padStart(2, '0');
-    const minutes = now.getUTCMinutes().toString().padStart(2, '0');
-    const seconds = now.getUTCSeconds().toString().padStart(2, '0');
-    const milliseconds = now.getUTCMilliseconds().toString().padStart(3, '0');
-    const formattedTime = `${hours}:${minutes}:${seconds}.${milliseconds}`;
-    document.getElementById('time').textContent = formattedTime;
-}
+  function displayCurrentTime() {
+            var currentTimeMilliseconds = new Date().getTime();
+            document.getElementById("time").textContent = currentTimeMilliseconds;
+        }
 
-updateUTC(); // Initial update
-setInterval(updateUTC, 100); 
+        
+        displayCurrentTime();
+
+      
+         setInterval(displayCurrentTime, 100);
